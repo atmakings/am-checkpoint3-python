9feb23f9a6504a523ead6ac440454bb093845b48
atmakings <atmakings@gmail.com>
2022-08-11 16:34:37 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,22 @@
+string = 'This is a string.'
+number = 100
+list = ['item1', 'item2', 'item3']
+boolean = True
+
+letters = string[0:3]
+
+list_element = list[0]
+
+number_2 = number + 10
+
+list_element_2 = list[2]
+
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+
+new_word = string[0:4].upper()
+new_sentence = new_word + string[4:]
+
+email = f'Thank you for purchasing {number} products'
+
+print('hello world')
